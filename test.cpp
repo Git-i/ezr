6ddb1bd3529b1b9c2dfbd4bd352947fe0d262c4c
Eric Nwogbo <ericis2007@gmail.com>
2024-08-11 00:33:41 +0100

    removed one_of

--- test.cpp
+++ test.cpp
@@ -1,14 +1,7 @@
-#include "include/one_of.hpp"
 #include <cstdint>
 #include <iostream>
 #include <string>
-using namespace ezr;
 enum class eText{
     U32, String
 };
-using Text = one_of<eText, uint32_t, std::string>;
-int main()
-{
-    Text t(eText::U32, "lmao");
-    std::cout << t.get<eText::String>() << std::endl;
-}
+int main(){}
